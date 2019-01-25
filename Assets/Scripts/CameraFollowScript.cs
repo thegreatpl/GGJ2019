76d808521f948e805fb3c6ac834209796e1e26c6
thegreatpl <imperialjustinian@gmail.com>
2019-01-25 20:51:26 +0000

    Ship movement basics.

---
+++ Assets/Scripts/CameraFollowScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollowScript : MonoBehaviour
+{
+    public GameObject FollowObject; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = FollowObject.transform.position; 
+    }
+}
---
+++ Assets/Scripts/Ship/ShipControlScript.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class ShipControlScript : MonoBehaviour
+{
+    public float Speed = 1f;
+
+    public float RotationSpeed = 0.1f;
+
+    public Rigidbody2D Rigidbody2D; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Rigidbody2D = GetComponent<Rigidbody2D>(); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var horizonatal = Input.GetAxis("Horizontal");
+        if (horizonatal < 0)
+            RotateClockwise();
+        if (horizonatal > 0)
+            RotateAntiClockwise();
+
+        var verticle = Input.GetAxis("Vertical");
+        if (verticle < 0)
+            Decelerate();
+        if (verticle > 0)
+            Accelerate(); 
+    }
+
+    void RotateClockwise()
+    {
+        Rigidbody2D.rotation += RotationSpeed; 
+    }
+
+    void RotateAntiClockwise()
+    {
+        Rigidbody2D.rotation -= RotationSpeed;
+
+    }
+
+    void Accelerate()
+    {
+        Rigidbody2D.AddForce(transform.up * Speed); 
+    }
+
+    void Decelerate()
+    {
+        Rigidbody2D.AddForce((transform.up * Speed) * -1);
+    }
+}
